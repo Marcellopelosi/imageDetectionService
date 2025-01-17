f157df0edb2323cbd854349eb4ba1f877973516a
Marcellopelosi <57409544+Marcellopelosi@users.noreply.github.com>
2025-01-17 12:15:37 +0100

    Update app.py

--- app.py
+++ app.py
@@ -46,5 +46,7 @@ def detect_objects():
     # Return detected objects and image
     return jsonify({"detected_objects": detected_objects, "image": img_str}), 200
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
